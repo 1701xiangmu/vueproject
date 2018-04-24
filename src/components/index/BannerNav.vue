5e956bbc2e9ba25a711459463e63b778b85b67ed
whl51361 <245385377@qq.com>
2018-04-24 16:36:02 +0800

    vue全部文件

---
+++ src/components/index/BannerNav.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="swiper-container Nav">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <div class="nav">
+          <ul class="nav-ul1">
+            <li class="nav-ul1-li1"><img src="../../assets/img/index-nav1.png"/><br><span>机票</span></li>
+            <li class="nav-ul1-li2"><img src="../../assets/img/index-nav2.png"/><br><span>酒店</span></li>
+            <li class="nav-ul1-li3"><img src="../../assets/img/index-nav3.png"/><br><span>机+酒</span></li>
+            <li class="nav-ul1-li4"><img src="../../assets/img/index-nav4.png"/><br><span>全球购</span></li>
+            <!--<cqhk-nav-banner-img key="index" :pro="n" v-for="(n,index) in imgNa.img01"></cqhk-nav-banner-img>-->
+          </ul>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="nav">
+          <ul class="nav-ul1">
+            <li class="nav-ul1-li2"><img src="../../assets/img/index-nav10.png"/><br><span>行程规划</span></li>
+            <li class="nav-ul1-li6"><img src="../../assets/img/index-nav11.png"/><br><span>包车游</span></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <!--<cqhk-nav-banner-img key="index" :pro="n" v-for="(n,index) in imgNa.img02"></cqhk-nav-banner-img>-->
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+  import Swiper from  "swiper";
+  import "../../../node_modules/swiper/dist/css/swiper.min.css";
+    export default {
+        name: "banner-nav",
+      mounted:function(){
+        var swiper = new Swiper('.Nav', {
+          pagination: {
+            el: '.swiper-pagination',
+          },
+          paginationClickable: true,
+//        slidesPerView: 1,
+//        spaceBetween: 20,
+//        centeredSlides: true,
+          autoplay: true,
+//        autoplayDisableOnInteraction: false,
+          //循环播放
+          loop: true
+        });
+
+      },
+    }
+</script>
+
+<style lang="scss" scoped>
+  /*----------------------------nav---------------------------------*/
+  .nav{
+    width:100%;
+    height:6.5rem;
+    .nav-ul1{
+      width:100%;
+      height:3.24rem;
+      display: flex;
+      display: -webkit-flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex-wrap: wrap;
+      .nav-ul1-li1 img{
+        background: #e29f37;
+      }
+      .nav-ul1-li2{
+        img{
+          background:#cc2827;
+        }
+      }
+      .nav-ul1-li3{
+        img{
+          background:#7542dd;
+        }
+      }
+      .nav-ul1-li4{
+        img{
+          background:#2ec961;
+        }
+      }
+      .nav-ul1-li5{
+        img{
+          background:#fbc800;
+        }
+      }
+      li{
+        width: 25%;
+        height:2.31rem;
+        position: relative;
+        text-align: center;
+        margin-top:0.56rem;
+        img{
+          width:1.48rem;
+          height:1.48rem;
+          border-radius: 50%;
+        }
+        span{
+          display: block;
+          margin-top:0.2rem;
+          font-size:0.3rem;
+        }
+      }
+    }
+
+    .nav-ul2-li6{
+      width:2.5rem;
+      img{
+        background: #1593e0;
+      }
+
+    }
+    .nav-ul2-li7{
+      width:2.2rem;
+      img{
+        background: #e545e5;
+      }
+
+    }
+    .nav-ul2-li8 img{
+      background: #15bbf9;
+    }
+    .nav-ul2-li9 img{
+      background: #ec4386;
+    }
+    .nav-ul2-li10 img{
+      background: #ff4c55;
+    }
+
+    li{
+      width:2.3rem;
+      height:2.31rem;
+
+      position: relative;
+      text-align: center;
+      img{
+        width:1.48rem;
+        height:1.48rem;
+        border-radius: 50%;
+        background: #e29f37;
+      }
+      span{
+        display: block;
+        margin-top:0.2rem;
+        font-size:0.3rem;
+      }
+    }
+
+  }
+  .swiper-container {
+    width: 100%;
+    position: relative;
+    background: white;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-size: 0.14rem;
+    color: #000;
+    margin: 0;
+    padding: 0; }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 0.14rem; }
+
+  .swiper-slide img {
+    width: 100%;
+    height: 100%; }
+</style>
---
+++ src/components/index/HeaderBanner.vue
@@ -0,0 +1,164 @@
+<template>
+  <header>
+    <div class="swiper-container Head">
+      <div class="swiper-wrapper">
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner1.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner2.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner3.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner4.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner5.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner6.jpg"/>-->
+        <!--</div>-->
+        <!--<div class="swiper-slide">-->
+          <!--<img src="../../assets/img/index-banner7.jpg"/>-->
+        <!--</div>-->
+        <header-banner-img  key="ind" :img="n" v-for="(n,ind) in imgList"></header-banner-img>
+      </div>
+      <!-- Add Pagination -->
+        <div class="swiper-pagination Btn"></div>
+    </div>
+
+  </header>
+</template>
+
+<script>
+  import HeaderBannnerImg from "./HeaderBannerImg";
+  import Swiper from  "swiper";
+  import "../../../node_modules/swiper/dist/css/swiper.min.css";
+  // import axios from 'axios';
+  export default {
+    //name: 'HeaderBannnerImg',
+    props:["imgList"],
+    components:{
+      HeaderBannnerImg
+    },
+    mounted:function(){
+      var swiper = new Swiper('.Head', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        paginationClickable: true,
+//        slidesPerView: 1,
+//        spaceBetween: 20,
+//        centeredSlides: true,
+        autoplay: true,
+//        autoplayDisableOnInteraction: false,
+        //循环播放
+        loop: true
+      });
+
+    },
+    // data(){
+    //   return {
+    //     imgList:[]
+    //   }
+    // },
+    // created:function(){
+    //   // axios.get('/msg')
+    //   //   .then(function (res) {
+    //   //     this.imgList=res.data.img;
+    //   //     console.log(res.data.img)
+    //   //   })
+    //   //   .catch(function (err) {
+    //   //     console.log(err);
+    //   //   });
+    //     this.btn_ajax();
+    // },
+    // methods:{
+    //   btn_ajax:function(){
+    //
+    //     this.$http({
+    //       method:'get',
+    //       url:'/msg',
+    //       params: {                    //需要发送的数据
+    //         // name: 'zhangwenwu2',
+    //         // age: '15'
+    //       }
+    //     }).then(function (res) {
+    //        var res = JSON.parse(res);
+    //
+    //        this.imgList=res.data.img;
+    //       console.log(res.data.img)
+    //     }).catch(function (err) {
+    //         console.log(err);
+    //       });
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+  /*-----------头部start---------------------*/
+  header {
+    display: flex;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: #eee;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #000;
+    margin: 0;
+    padding: 0; }
+
+  .swiper-container {
+    width: 100%;
+    height: 100%; }
+
+  .swiper-pagination .swiper-pagination-bullet-active {
+    background: #0bca97; }
+
+  .Btn .swiper-pagination-bullet-active {
+    background: #9e9e9e; }
+
+  .swiper-slide {
+    width: 100%;
+    height: 4.88889rem;
+    text-align: center;
+    font-size: 0.25rem;
+    background: #fff;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center; }
+  .swiper-slide img {
+    width: 100%;
+    height: 100%; }
+  .swiper-container {
+    width: 100%;
+    position: relative;
+    background: white;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-size: 0.14rem;
+    color: #000;
+    margin: 0;
+    padding: 0; }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 0.14rem; }
+
+  .swiper-slide img {
+    width: 100%;
+    height: 100%; }
+
+</style>
